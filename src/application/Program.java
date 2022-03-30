db1555c8415b5411d513278f7c3d046a75c1fc17
MarVinCan <marcus.v.e.candido@gmail.com>
2022-03-30 17:54:50 -0300

    Implementação do metodo

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,10 +1,30 @@
 package application;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Locale;
+
+import entities.Product;
+import model.service.ProductService;
+
 public class Program {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		Locale.setDefault(Locale.US);
+		List<Product> list = new ArrayList<>();
+		
+		list.add(new Product("Tv", 900.00));
+		list.add(new Product("Mouse", 50.00));
+		list.add(new Product("Tablet", 350.50));
+		list.add(new Product("HD Case", 80.90));
+		
+		ProductService ps = new ProductService();
+		
+		double sum =ps.filterSum(list, p -> p.getName().charAt(0) == 'T');
+		
+		System.out.println("Sum =" + String.format(" %.2f", sum));
 
 	}
 
 }
+
--- src/model/service/ProductService.java
+++ src/model/service/ProductService.java
@@ -1,5 +1,20 @@
 package model.service;
 
+import java.util.List;
+import java.util.function.Predicate;
+
+import entities.Product;
+
 public class ProductService {
+	
+	public double filterSum(List<Product> list, Predicate<Product> 	criteria) {
+		double sum  = 0.0;
+		for(Product p : list) {
+			if(criteria.test(p)) {
+				sum += p.getPrice();
+			}
+		}
+		return sum;
+	}
 
 }
